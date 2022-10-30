42168e21c4fd803d3f89937ab269e9f13c6bca87
Vadim <fishermanvadik@gmail.com>
2022-10-30 03:53:07 +0300

    Настройка модуля

---
+++ Controller.py
@@ -0,0 +1,8 @@
+from Sorty import sort,get_da
+from Veiw import get_number_operation,get_data
+from Read_and_Write import read_file,write_file
+from Main import sort_data
+
+
+def button():
+    return sort_data(get_number_operation,read_file,write_file,sort,get_da,get_data)
